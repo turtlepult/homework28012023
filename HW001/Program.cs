d1b8688125a69ce0654023827c9eeaaad4e2da5b
Ivan_Ladygin <ivan_ladiygin@mail.ru>
2023-02-04 18:54:59 +0500

    perhaps a working program has been created 002

---
+++ HW001/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+Console.Write("Введите k1: ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+ 
+var x = (b2-b1) / (k1 - k2);
+var y = k1 * x + b1;
+ 
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
---
+++ HW002/Program.cs
@@ -0,0 +1,41 @@
+﻿// Написать программу масштабирования фигуры
+Console.WriteLine("введите количество вершин: ");
+int n = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine("введите коофициент маштабирования: ");
+int k = int.Parse(Console.ReadLine()??"0");
+int[] coor = new int[n*2];
+int count = 1; //счетчик для подсчета текущего х, у
+for (int i = 0; i < n*2; i++)
+    { 
+        if (i%2==0) // для разбивки на х, у 
+        {
+        Console.WriteLine("введите координату х"+count+": ");
+        int temp = int.Parse(Console.ReadLine()??"0");
+        coor[i] = temp;
+        }
+        else
+        {
+        Console.WriteLine("введите координату y"+count+": ");
+        int temp = int.Parse(Console.ReadLine()??"0");
+        coor[i] = temp; 
+        count++;
+        }
+    }
+for (int i = 0; i < coor.Length; i++)
+{
+    coor[i]=coor[i]*k;
+}
+Console.WriteLine();
+count = 1;//сбрасываем счетчик
+for (int i = 0; i < n*2; i++)
+    { 
+        if (i%2==0) // для разбивки на х, у 
+        {
+        Console.WriteLine("координа х"+count+": "+coor[i]);
+        }
+        else
+        {
+        Console.WriteLine("координа y"+count+": "+coor[i]);
+        count++;
+        }
+    }
